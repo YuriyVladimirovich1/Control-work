eb3d04706aea87681b8b206bb8ba8aacba033453
Yuriy V <a.yuriy.v@mail.ru>
2022-07-24 13:03:31 +0300

    Worked on the code

--- control task/Program.cs
+++ control task/Program.cs
@@ -5,7 +5,7 @@ void ArraysOfStrings(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+      if(array1[i].Length <= 3)
         {
         array2[count] = array1[i];
         count++;
